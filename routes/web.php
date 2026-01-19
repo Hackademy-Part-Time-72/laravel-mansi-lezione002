8d26425c4a541c330ce818335fd0359c2623fb10
francesco mansi <f.mansi@socialwebsolutions.it>
2026-01-19 19:27:52 +0100

    Aggiunta gestione errore 404

--- routes/web.php
+++ routes/web.php
@@ -6,3 +6,11 @@ Route::get('/', function () {
     return view('welcome');
     //modificato
 });
+
+Route::get('/chi-sono', function () {
+    return view('about');
+});
+
+Route::get('/servizi', function () {
+    return view('services');
+});
